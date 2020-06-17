7da7535c747aa7ba2eb9a51d05be098215a59004
joshua1535 <00176020@uca.edu.sv>
2020-06-17 13:00:33 -0600

    commit

---
+++ Labo7/ejercicio4.cpp
@@ -0,0 +1,50 @@
+#include "iostream"
+using namespace std;
+
+int Leer_arreglo1(int arreglo1[],int n){ 
+
+    for(int i = 0; i<n; i++){
+        cout << "Ingrese el valor en la posicion " << i << " del arreglo 1: ";
+        cin >> arreglo1[i];
+    }return arreglo1[n];
+}
+// Se realizan las funciones que pediran los elementos que se sumaran 
+int Leer_arreglo2(int arreglo2[],int n){
+
+    for(int i = 0; i<n; i++){
+        cout << "Ingrese el valor en la posicion " << i << " del arreglo 2: ";
+        cin >> arreglo2[i];
+    }return arreglo2[n];
+}
+
+int Suma_arreglos(int arreglo1[],int arreglo2[],int n, int arreglosum[]){ 
+
+    for(int i =0; i<n; i++){ //se suman los elementos de ambos arrayas y se almacena en un tercero
+        arreglosum[i] = arreglo1[i] + arreglo2[i];
+    } return arreglosum[n];
+}
+
+int Desplegar_array(int arreglosum[], int n){ //funcion que solamente imprime la suma ya realizada anteriormente
+
+    cout << "La suma de los arreglos fue de: ";
+    for (int i= 0; i<n; i++){
+        cout << arreglosum[i] << "\t";
+    }
+}
+
+int main(void){
+    int n; //cantidad de elementos para ambos arrays
+
+    cout << "Ingrese el numero de elementos que formaran los arreglos: ";
+    cin >>n;
+
+    int arreglo1[n];    // se declaran los arryas con la cantidad de elementos escogidos por el usuario
+    int arreglo2[n];
+    int arreglosum[n];
+
+    Leer_arreglo1(arreglo1,n);
+    cout << endl;
+    Leer_arreglo2(arreglo2,n);
+    Suma_arreglos(arreglo1,arreglo2,n,arreglosum);
+    Desplegar_array(arreglosum,n);
+}
---
+++ Labo7/ejercicio5.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+using namespace std;
+
+int main(void){
+
+    cout << "A continuacion se imprimiran los primeros 100 numeros impares de manera decreciente" << endl;
+    int num = 199;
+    int impar[num];
+
+    for(int i=0; i<=num; i++){
+        impar[i]= i;
+    }
+
+    for(int i=199; i >=1; i-=2){
+        cout << impar[i] << "\t";
+    }
+}
